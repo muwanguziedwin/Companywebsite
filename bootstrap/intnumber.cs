2c45cefb38499ee15b5f1b124cf08b97c50046dd
muwanguziedwin <muwanguziedwin19@gmail.com>
2022-10-24 09:31:12 +0300

    kk

---
+++ bootstrap/intnumber.cs
@@ -0,0 +1,6 @@
+int number,result;
+number = 5;
+Console.WriteLine("Result is {0} when 100 is multiplied by {1}", 
+result,number);
+result = 150 / number;
+Console.WriteLine("Result is {0} when 150 is divided by {1}", +result, number);
